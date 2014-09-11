1310825c7031e59f91f6288fedafd24158870e9c
robertsone <rufrrufr@yahoo.com>
2014-09-11 08:25:00 -0400

    Finished

--- Furniture/Program.cs
+++ Furniture/Program.cs
@@ -9,35 +9,34 @@ namespace Furniture
     {
         static void Main(string[] args)
         {
-            System.Console.Writeline("What wood type do you want?\n");
+            int unit;
+            System.Console.Write("What wood type do you want?\n\nCom\tName\t\tPrice\n\n[p]\tPine\t\t$100\n[o]\tOak\t\t$225\n[m]\tMahogany\t$310\n");
+            string response = Console.ReadLine().ToLower();
+            System.Console.WriteLine("How many units of wood?");
+            string amount = Console.ReadLine(); unit = Convert.ToInt32(amount);
+            if (unit >0)
+            {
+                if (response == "p")
+                {
+                    System.Console.WriteLine("You have bought " + unit + " Pine for $" + unit * 100);
+                }
+                else if (response == "o")
+                {
+                    System.Console.WriteLine("You have bought " + unit + " Oak for $" + unit * 225);
+                }
 
+                else if (response == "m")
+                {
+                    System.Console.WriteLine("You have bought " + unit + " Mahogany for $" + unit*310);
+                }
+                else
+                {
+                    System.Console.WriteLine("That isn't a type of wood, reopen the program and try typing the lowercase letter p, o, or m.");
+                }
+            }
+            
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+            
 
 
 
---
+++ Payroll/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Payroll
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            System.Console.WriteLine("What is your hourly wage?");int pay = Convert.ToInt32(System.Console.ReadLine());
+            System.Console.WriteLine("How many hours weekly do you spend in a pointless cubicle"); int hours = Convert.ToInt32(System.Console.ReadLine());
+            double totalpay = pay * hours;
+
+            if (totalpay >= 300.01) System.Console.WriteLine("\nGross pay\t\tWithhold Percent\t\tTotal\n\n" + totalpay + "\t\t\t%12\t\t\t\t"+(totalpay-(totalpay*0.12)));
+            else System.Console.WriteLine("\nGross pay\t\tWithhold Percent\t\tTotal\n\n" + totalpay + "\t\t\t%10\t\t\t\t" + (totalpay - (totalpay * 0.10)));
+                
+
+
+        }
+    }
+}
