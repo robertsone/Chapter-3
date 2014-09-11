edbc0e6248fce918b4d4a1b8fc73ce75908ee69f
robertsone <rufrrufr@yahoo.com>
2014-09-11 08:44:30 -0400

    Added cool things

---
+++ rpg/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace rpg
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string location = "Startup";
+
+            System.Console.Write("Name Your Player:");
+            string Name = System.Console.ReadLine();
+
+            while (true)
+            {
+
+
+                if (location == "Startup")
+                {
+                    System.Console.WriteLine(Name + ", What class do you want?\nRouge\tMage\tKnight");
+                    string Player_Class = System.Console.ReadLine();
+                    if (Player_Class == "Rouge") ;
+                    else if (Player_Class == "Mage") ;
+                    else if (Player_Class == "Knight") ;
+                    else System.Console.WriteLine("Sorry, thats not a class");
+                }
+
+
+            }
+        }
+    }
+}
