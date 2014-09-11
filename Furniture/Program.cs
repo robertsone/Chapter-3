25ac8c5e9090d9354ae079261baf164c0472a5a7
robertsone <rufrrufr@yahoo.com>
2014-09-11 08:33:16 -0400

    done

--- Furniture/Program.cs
+++ Furniture/Program.cs
@@ -10,11 +10,15 @@ namespace Furniture
         static void Main(string[] args)
         {
             int unit;
+            
             System.Console.Write("What wood type do you want?\n\nCom\tName\t\tPrice\n\n[p]\tPine\t\t$100\n[o]\tOak\t\t$225\n[m]\tMahogany\t$310\n");
             string response = Console.ReadLine().ToLower();
+            
             System.Console.WriteLine("How many units of wood?");
             string amount = Console.ReadLine(); unit = Convert.ToInt32(amount);
-            if (unit >0)
+
+
+            if (unit > 0)
             {
                 if (response == "p")
                 {
@@ -27,16 +31,16 @@ namespace Furniture
 
                 else if (response == "m")
                 {
-                    System.Console.WriteLine("You have bought " + unit + " Mahogany for $" + unit*310);
+                    System.Console.WriteLine("You have bought " + unit + " Mahogany for $" + unit * 310);
                 }
                 else
                 {
                     System.Console.WriteLine("That isn't a type of wood, reopen the program and try typing the lowercase letter p, o, or m.");
                 }
             }
-            
 
-            
+
+
 
 
 
